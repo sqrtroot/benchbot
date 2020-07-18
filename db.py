ff5f9d03ece7be095219d72e0fce637e70e30179
Robert <robert@sqrtroot.com>
2020-07-19 00:49:28 +0200

    Bot now downloads files and updates db

--- db.py
+++ db.py
@@ -1,31 +1,131 @@
 import sqlalchemy
-from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
-from sqlalchemy_aio import ASYNCIO_STRATEGY
-from typing import Optional
-import asyncio
+from tabulate import tabulate
+from sqlalchemy import create_engine, Column, Integer, String, MetaData, Float, Boolean, ForeignKey, and_, \
+    UniqueConstraint, or_
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship, Session as SessionT
+from contextlib import contextmanager
+from typing import Optional, List, Union, Callable, Coroutine
+from inspect import iscoroutinefunction
 
-engine = create_engine('sqlite:///challenges.db', echo=True, strategy=ASYNCIO_STRATEGY)
+SessionT = SessionT
+
+# string types see:
+# https://mypy.readthedocs.io/en/latest/common_issues.html#using-classes-that-are-generic-in-stubs-but-not-at-runtime
+
 meta = MetaData()
-
 Result = sqlalchemy.engine.ResultProxy
+Base = declarative_base(metadata=meta)
 
-contenders = Table(
-    'contender', meta,
-    Column('id', Integer, primary_key=True),
-    Column('name', String),
-)
+engine = create_engine('sqlite:///challenges.db', echo=True)
+Session: sessionmaker = sessionmaker()
+Session.configure(bind=engine)
 
-challenges = Table(
-    'challenge', meta,
-    Column('id', Integer, primary_key=True),
-    Column('name', String),
-)
 
-# benchmark = Table()
+def with_session(fn: Union[Callable, Coroutine]):
+    def sess_func(*args, **kwargs):
+        session: SessionT = Session()
+        print("Hey")
+        try:
+            fn(session, *args, **kwargs)
+            session.commit()
+        except Exception as e:
+            session.rollback()
+            raise e
+        finally:
+            session.close()
+
+    async def async_sess_func(*args, **kwargs):
+        session: SessionT = Session()
+        print("Hey")
+        try:
+            await fn(session, *args, **kwargs)
+            session.commit()
+        except Exception as e:
+            session.rollback()
+            raise e
+        finally:
+            print('closing session')
+            session.close()
+
+    if not iscoroutinefunction(fn):
+        sess_func.__name__ = fn.__name__
+        return sess_func
+    else:
+        async_sess_func.__name__ = fn.__name__
+        return async_sess_func
+
+
+class Contender(Base):
+    __tablename__ = 'contender'
+    id            = Column(Integer, primary_key=True, nullable=False)
+    name          = Column(String, nullable=False)
+    benchmarks    = relationship('Benchmark', back_populates='contender')
+
+
+class Challenge(Base):
+    __tablename__  = 'challenge'
+    __table_args__ = (UniqueConstraint("name", "active"))
+    id             = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name           = Column(String, nullable=False)
+    active         = Column(Boolean, nullable=False)
+    benchmarks     = relationship('Benchmark', back_populates='challenge')
+
+
+class Benchmark(Base):
+    __tablename__  = 'benchmark'
+    __table_args__ = (
+        UniqueConstraint("id", "hash"),
+    )
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    contender_id: 'Column[ForeignKey]' = Column(ForeignKey(Contender.id),
+                                                nullable=False)
+    challenge_id: 'Column[ForeignKey]' = Column(ForeignKey(Challenge.id),
+                                                nullable=False)
+    contender = relationship('Contender', back_populates='benchmarks')
+    challenge = relationship('Challenge', back_populates='benchmarks')
+    hash      = Column(String(length=32))
+    error     = Column(String)
+    min_time  = Column(Float)
+    avg_time  = Column(Float)
+    max_time  = Column(Float)
+    bin_size  = Column(Float)
+
+    def result_format(self):
+        if not self.error or not self.avg_time:
+            return "Waiting for results"
+        if self.error:
+            rslt = [['error', self.error]]
+        else:
+            rslt = [
+                ['Minimum time', self.min_time],
+                ['Average time', self.avg_time],
+                ['Maximum time', self.max_time],
+            ]
+        return tabulate([
+            ['Submitted by', self.contender.name],
+            ['hash', self.hash],
+            ['binary_size', self.bin_size],
+            *rslt])
+
 
 connection: Optional[sqlalchemy.engine.Connection] = None
 
-async def init_db():
-    meta.create_all(engine.sync_engine)
+
+def init_db() -> None:
+    meta.create_all(engine.engine)
     global connection
-    connection = await engine.connect()
+    connection = engine.connect()
+
+
+def find_author(session, author_id) -> Optional[Contender]:
+    result: sqlalchemy.orm.Query = session.query(
+        Contender).filter(Contender.id == author_id)
+    return result.first()
+
+
+def get_challenge(session, name) -> Optional[Challenge]:
+    result = Session().query(Challenge).filter(
+        and_(Challenge.active,
+             or_(Challenge.name == name, Challenge.id == name)))
+    return result.first()
--- main.py
+++ main.py
@@ -1,28 +1,108 @@
 import discord
 from discord.ext.commands import Bot, Context
-from consts import TOKEN, CODE_CHANNEL
+from discord.ext.tasks import loop, Loop
+from consts import TOKEN, BINARY_DIR
 import re
-import db
+from db import *
 import asyncio
+import psutil
+from tabulate import tabulate
+from hashlib import md5
+
+EMPTY_CHAR = '﻿'
+
 awaits = lambda *f: asyncio.wait([*f])
 
 bot = Bot(command_prefix=">")
+
+
+@loop(seconds=1, loop=bot.loop)
+async def status():
+    print('hoi')
+    bot_status = discord.Status.dnd
+    await bot.change_presence(status=bot_status,
+                              activity=discord.Activity(
+                                  type=discord.ActivityType.playing,
+                                  name=f'Usage: {psutil.cpu_percent()}%'))
+
 
 @bot.command()
 async def ping(ctx: Context):
     await ctx.send(f"<@{ctx.author.id}> pong")
 
+
 @bot.command()
-async def start(ctx:Context):
-    req = db.contenders.select().where(db.contenders.c.id == ctx.author.id)
-    result: db.Result = await db.connection.execute(req)
-    found = await result.scalar()
-    if found:
-        await ctx.send(f"{ctx.author.mention} You've already registered")
+@with_session
+async def start(session: SessionT, ctx: Context):
+    author = find_author(session, ctx.author.id)
+    if author:
+        await ctx.send(f"{ctx.author.mention} You have already registered")
     else:
-        ins = db.contenders.insert().values(id=ctx.author.id, name=ctx.author.name)
-        await awaits(db.connection.execute(ins),
-                     ctx.send(f"Welcome to the challenges {ctx.author.mention}"))
+        session.add(Contender(id=ctx.author.id, name=ctx.author.name))
+        await ctx.send(f'Welcome to the challenges {ctx.author.mention}')
+
+
+@bot.command(aliases=['bench', 'b'])
+@with_session
+async def benchmark(session: SessionT, ctx: Context):
+    if not ctx.message.attachments:
+        await ctx.send(f"{ctx.author.mention} Try attaching a binary!")
+        return
+
+    author = find_author(session, ctx.author.id)
+    if not author:
+        await ctx.send(
+            f"{ctx.author.mention} You have not yet registered for the challenge!"
+        )
+        return
+    attachment: discord.Attachment = ctx.message.attachments[0]
+
+    max_size = 1024 * 1024 * 10  # 10 MiB
+    if attachment.size > max_size:
+        await ctx.send(
+            f"{ctx.author.mention} I currently don't accept binaries larger than {max_size // 1024 ** 2} MiB"
+        )
+        return
+
+    challenge = get_challenge(session, attachment.filename)
+    if not challenge:
+        await ctx.send(
+            f"{ctx.author.mention} I haven't heard about that challenge. Maybe check the active challenges"
+        )
+        return
+
+    data = await attachment.read()
+    hash = md5()
+    hash.update(data)
+
+    already_benched = session.query(Benchmark).filter(Benchmark.hash == hash.hexdigest())
+    print(already_benched.first())
+    if bench_result := already_benched.first():
+        await ctx.send(f'Already benchmarked this file:\n```{bench_result.result_format()}```')
+        return
+
+    bench = Benchmark(challenge=session.merge(challenge),
+                      contender=session.merge(author),
+                      hash=hash.hexdigest(),
+                      bin_size=attachment.size)
+    session.add(bench)
+    session.flush()
+
+    filename = f'{BINARY_DIR}/{bench.id}'
+    with open(filename, 'wb') as f:
+        f.write(data)
+
+
+@bot.command()
+@with_session
+async def challenges(session: SessionT, ctx: Context):
+    chlngs = []
+    for challenge in session.query(Challenge).filter(Challenge.active).all():
+        chlngs.append((challenge.id, challenge.name))
+    if chlngs:
+        await ctx.send(f'{EMPTY_CHAR}\n```{tabulate(chlngs, headers=["Id", "Name"], tablefmt="fancy_grid")}```')
+    else:
+        await ctx.send("No active challenges at the moment")
 
 
 @bot.listen()
@@ -33,5 +113,5 @@ async def on_message(message: discord.Message):
             pass
 
 
-bot.loop.create_task(db.init_db())
+init_db()
 bot.run(TOKEN)
